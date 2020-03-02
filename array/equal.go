afccc2f5828729bbe1c2391aff3e7f8218183645
532A <101133899+532A@users.noreply.github.com>
2020-03-02 07:56:10 +0000

    Translated by lars

--- array/equal.go
+++ array/equal.go
@@ -2,4 +2,9 @@ package array
 
 // EqFloat64 compares two arrays.
 func EqFloat64(v1, v2 []float64) bool {
-	if len(v1) != len(v
+	if len(v1) != len(v2) {
+		return false
+	}
+
+	for i, v1i := range v1 {
+		if 
