723b633968dc980c744d34ada96a2c7d8c62354f
532A <101133899+532A@users.noreply.github.com>
2020-06-20 20:19:57 +0000

    Record changes.

--- bayes/calculate_test.go
+++ bayes/calculate_test.go
@@ -6,4 +6,7 @@ func TestProbability(t *testing.T) {
 	categoryDog, categoryCat := Category(1), Category(2)
 
 	categoryBrownHair, categoryBlondeHair := Category(1), Category(2)
-	categoryBlueEyes, categoryBrownEyes
+	categoryBlueEyes, categoryBrownEyes := Category(3), Category(4)
+
+	tests := []struct {
+		n
