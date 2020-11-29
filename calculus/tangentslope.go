6be2efa849741ae03bd2e96d97ad5e639214ef6c
532A <101133899+532A@users.noreply.github.com>
2020-11-29 06:03:26 +0000

    dev-master

--- calculus/tangentslope.go
+++ calculus/tangentslope.go
@@ -1,3 +1,4 @@
 package calculus
 
-// TangentSlope returns the slope of the line at point x by measuring the slope from x - d t
+// TangentSlope returns the slope of the line at point x by measuring the slope from x - d to x.
+func TangentSlope(x, d float64, f func(x float64) (y float64)) Li
