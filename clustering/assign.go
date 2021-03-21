7af9d208874e186599542759fcf91d9e257a9dbc
532A <101133899+532A@users.noreply.github.com>
2021-03-21 13:04:55 +0000

    sign in

--- clustering/assign.go
+++ clustering/assign.go
@@ -7,4 +7,7 @@ import (
 // Assign vectors to their clusters.
 func Assign(data []Vector, assignment []int) ([]Cluster, error) {
 	if len(assignment) != len(data) {
-		return nil, errors.New("assignment must equal the am
+		return nil, errors.New("assignment must equal the amount of input")
+	}
+	clusters := 0
+	for _, v := range
