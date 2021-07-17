0254376675d390ce22e3f969beecbb3f5a16593a
532A <101133899+532A@users.noreply.github.com>
2021-07-17 09:26:16 +0000

    Added more docs

--- clustering/centroid_test.go
+++ clustering/centroid_test.go
@@ -46,4 +46,4 @@ func TestCentroid(t *testing.T) {
 		actual, err := Centroid(test.data)
 		if test.expectedError != nil {
 			if err.Error() != test.expectedError.Error() {
-	
+				t.Fatalf("%s: expected error '%v', but got '%v'", test.
