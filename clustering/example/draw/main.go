cfb2448737a1e35e8078e41caf164cb8700372eb
532A <101133899+532A@users.noreply.github.com>
2021-08-24 07:05:32 +0000

    minor refactor.

--- clustering/example/draw/main.go
+++ clustering/example/draw/main.go
@@ -50,3 +50,53 @@ func main() {
 	if err != nil {
 		fmt.Println("Error assigning data to clusters: ", err)
 		os.Exit(-1)
+	}
+
+	// Convert them to scatter inputs (something that implements the XYer interface).
+	for i, cluster := range clusters {
+		scatter := convert2DVectorToPlotterXY(cluster)
+		// Add them to the chart.
+		err = addScatters(p, i, strconv.Itoa(i+1), scatter)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Save the plot to a PNG file.
+	if err := p.Save(15*vg.Centimeter, 15*vg.Centimeter, "points.png"); err != nil {
+		panic(err)
+	}
+}
+
+func convert2DVectorToPlotterXY(v []clustering.Vector) plotter.XYs {
+	pts := make(plotter.XYs, len(v))
+	for i := 0; i < len(v); i++ {
+		pts[i] = xy{
+			X: v[i][0],
+			Y: v[i][1],
+		}
+	}
+	return pts
+}
+
+type xy struct {
+	X, Y float64
+}
+
+func random2DVectors(n int) []clustering.Vector {
+	op := make([]clustering.Vector, n)
+	for i := 0; i < n; i++ {
+		v := make(clustering.Vector, 2)
+		randomise(v, -10, 10)
+		op[i] = v
+	}
+	return op
+}
+
+func randomise(v []float64, min, max int) {
+	for i := 0; i < len(v); i++ {
+		v[i] = float64(rand.Intn(max-min) + min)
+	}
+}
+
+func addScatters(plt *plot.Plot, index int, name string, xyers plotter.XYs) error {
