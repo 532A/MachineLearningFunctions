c97da09a365e5623d43c1bf68731cbf14f9a9a38
532A <101133899+532A@users.noreply.github.com>
2021-10-01 16:02:56 +0000

    ToDo

--- clustering/kmeans.go
+++ clustering/kmeans.go
@@ -20,4 +20,4 @@ func KMeans(data []Vector, n int, d distance.Function) (assignment []int, err er
 		return nil, errors.New("KMeans: data cannot be empty")
 	}
 
-	//
+	// Assign data to random clusters, but make sure every cl
