d6c04656f54c77f635c8ab6e87967ef83a02bc6b
532A <101133899+532A@users.noreply.github.com>
2022-01-07 17:36:58 +0000

    Update sbt to 1.4.4

--- clustering/kmeans_test.go
+++ clustering/kmeans_test.go
@@ -112,4 +112,11 @@ func TestKMeans(t *testing.T) {
 				}
 				if !found {
 					t.Errorf("%s: expected cluster to contain indices %v, but it contained %v", test.name,
-						expectedCluster, actualCluste
+						expectedCluster, actualCluster)
+				}
+			}
+		}
+	}
+}
+
+func getClusters(data []
