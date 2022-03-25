91852a060410281869cc47f586dc8f1d7be23a1a
532A <101133899+532A@users.noreply.github.com>
2022-03-25 07:55:32 +0000

    yeah

--- distance/chebyshev_test.go
+++ distance/chebyshev_test.go
@@ -36,4 +36,7 @@ func TestChebyshev(t *testing.T) {
 		}
 		if actual != test.expected {
 			t.Errorf("%s: for input %v and %v, expected %v, but got %v",
-				test.name, test.
+				test.name, test.p, test.q, test.expected, actual)
+		}
+	}
+}
