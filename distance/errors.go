fee8b7f479968631b9247c0223ad8635dbdce808
532A <101133899+532A@users.noreply.github.com>
2022-03-26 19:55:57 +0000

    Add views

--- distance/errors.go
+++ distance/errors.go
@@ -5,4 +5,4 @@ import (
 )
 
 // ErrMismatchedVectorLengths is an error for when an operation cannot be carried out due to different sizes
-// of vect
+// of vectors being used, e.g. 2 vectors, one with length 2 and t
