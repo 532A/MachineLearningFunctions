5c53e1ae2fd8f6f933064c3d7aa1e088836fa533
532A <101133899+532A@users.noreply.github.com>
2022-03-27 13:46:29 +0000

    null fix

--- distance/errors.go
+++ distance/errors.go
@@ -6,4 +6,4 @@ import (
 
 // ErrMismatchedVectorLengths is an error for when an operation cannot be carried out due to different sizes
 // of vectors being used, e.g. 2 vectors, one with length 2 and the other of length 3 cannot be compared.
-var ErrMismatched
+var ErrMismatchedVectorLengths = errors.New("distance: mismatched vector lengths
