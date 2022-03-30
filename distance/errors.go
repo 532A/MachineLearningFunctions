2e93aade20d9fd6cb652bad6afec4fed6dc49294
532A <101133899+532A@users.noreply.github.com>
2022-03-30 13:27:21 +0000

    update typescript

--- distance/errors.go
+++ distance/errors.go
@@ -9,4 +9,4 @@ import (
 var ErrMismatchedVectorLengths = errors.New("distance: mismatched vector lengths")
 
 // ErrZeroLengthVector is an error for when an input has a zero length vector - i.e. there is nothing to compare.
-var ErrZeroLengthVector = errors
+var ErrZeroLengthVector = errors.New("distance: zero length 
