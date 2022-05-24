ff0d73ea9273430853fd6b6af3f0845c448bcb99
532A <101133899+532A@users.noreply.github.com>
2022-05-24 22:58:04 +0000

    Minor javadoc fix

--- distance/manhattan.go
+++ distance/manhattan.go
@@ -9,3 +9,9 @@ import (
 func Manhattan(p []float64, q []float64) (d float64, err error) {
 	if err = validateInputs(p, q); err != nil {
 		return
+	}
+	for i, pi := range p {
+		d += math.Abs(pi - q[i])
+	}
+	return
+}
