616bb050088b4e061ebf7e6562c6205f7c1fa3c8
532A <101133899+532A@users.noreply.github.com>
2022-06-16 19:50:04 +0000

    Update summary.md

--- distance/meansquare_test.go
+++ distance/meansquare_test.go
@@ -20,3 +20,38 @@ func TestMeanSquare(t *testing.T) {
 		},
 		{
 			name:     "One above",
+			p:        []float64{1, 2, 3, 4, 5},
+			q:        []float64{2, 3, 4, 5, 6},
+			expected: 1, // Average error is 1.
+		},
+		{
+			name:     "Two below",
+			p:        []float64{2, 3, 4, 5, 6},
+			q:        []float64{0, 1, 2, 3, 4},
+			expected: 4, // Average squared error is 4.
+		},
+		{
+			name:     "Same number twice",
+			p:        []float64{0, -1},
+			q:        []float64{9, 8},
+			expected: 81,
+		},
+		{
+			name:     "More complex",
+			p:        []float64{1, 2, 3},
+			q:        []float64{2, 12, 14},
+			expected: (1 + 100 + 121) / 3, // Square root of average squared error is 3.
+		},
+	}
+
+	for _, test := range tests {
+		actual, err := MeanSquare(test.p, test.q)
+		if err != nil {
+			t.Fatalf("%s: %v", test.name, err)
+		}
+		if actual != test.expected {
+			t.Errorf("%s: for input %v and %v, expected %v, but got %v",
+				test.name, test.p, test.q, test.expected, actual)
+		}
+	}
+}
---
+++ distance/rootmeansquare.go
@@ -0,0 +1,13 @@
+
+package distance
+
+import (
+	"math"
+)
+
+// RootMeanSquare calculates the root mean square (RMS) distance between two input vectors.
+func RootMeanSquare(p []float64, q []float64) (r float64, err error) {
+	r, err = MeanSquare(p, q)
+	r = math.Sqrt(r)
+	return
+}
