03d694ddfecd002252d0ceb210dc07d3d1f9da25
532A <101133899+532A@users.noreply.github.com>
2022-08-10 13:08:44 +0000

    more functions

--- random/random.go
+++ random/random.go
@@ -16,4 +16,6 @@ func Float64(min, max float64) float64 {
 
 // Float64Vector returns an array of float64 values min and max.
 func Float64Vector(min, max float64, size int) []float64 {
-	op :=
+	op := make([]float64, size)
+	for i := 0; i < size; i++ {
+		op
