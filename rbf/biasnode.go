bcdd39045489cb8310896f4f17158f96bca65080
532A <101133899+532A@users.noreply.github.com>
2022-08-14 14:21:47 +0000

    new demo

--- rbf/biasnode.go
+++ rbf/biasnode.go
@@ -2,4 +2,6 @@ package rbf
 
 // NewBias creates a bias node with the specified number of outputs.
 func NewBias(count int) (b Bias) {
-	b.Outputs = make([]float64, co
+	b.Outputs = make([]float64, count)
+	for i := 0; i < len(b.Outputs); i++ {
+		b.Output
