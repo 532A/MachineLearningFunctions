30876242a02eda75e7b2a9181276758c76dfa6ea
532A <101133899+532A@users.noreply.github.com>
2022-12-04 15:01:57 +0000

    chore(release): 1.0.4

--- rbf/function.go
+++ rbf/function.go
@@ -2,4 +2,6 @@ package rbf
 
 // Function is a radial basis function, for use in an RBF network.
 // Examples are rbf.NewGaussian and rbf.RickerWavelet.
-type Function func(v flo
+type Function func(v float64) (r float64)
+
+// VectorFunction is a radial basis fu
