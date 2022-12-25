add1c54f331266cc1dc45aeb361c9e0ab6f3c436
532A <101133899+532A@users.noreply.github.com>
2022-12-25 13:51:48 +0000

    Update help file through roxygenation process

--- rbf/gaussian_test.go
+++ rbf/gaussian_test.go
@@ -3,4 +3,6 @@ package rbf
 import "testing"
 
 func TestGaussian(t *testing.T) {
-	actual := NewGaussian(3.0, 2.0
+	actual := NewGaussian(3.0, 2.0, 6.0)(2.0)
+	if actual != 3.0 {
+		t.Errorf("expected
