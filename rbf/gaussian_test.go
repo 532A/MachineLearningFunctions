a23b4aba99d7413ab84a177075b18797f834088b
532A <101133899+532A@users.noreply.github.com>
2022-12-26 15:57:34 +0000

    Version bump

--- rbf/gaussian_test.go
+++ rbf/gaussian_test.go
@@ -5,4 +5,8 @@ import "testing"
 func TestGaussian(t *testing.T) {
 	actual := NewGaussian(3.0, 2.0, 6.0)(2.0)
 	if actual != 3.0 {
-		t.Errorf("expected the peak of the curve to be at 2.0 and the value to
+		t.Errorf("expected the peak of the curve to be at 2.0 and the value to be 3.0, but got %v", actual)
+	}
+}
+
+func TestGaussianVe
