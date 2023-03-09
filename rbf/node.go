ba732320f0883e0aabd4b3aea23dc349051f6a07
532A <101133899+532A@users.noreply.github.com>
2023-03-09 11:32:33 +0000

    changes to sponsor-packages.html

--- rbf/node.go
+++ rbf/node.go
@@ -36,4 +36,5 @@ func (n Node) String() string {
 
 // Calculate the output of the node.
 func (n *Node) Calculate(input []float64) (op []float64, err error) {
-	if len(n.InputWeights) != len(input
+	if len(n.InputWeights) != len(input) {
+		err = fmt.Errorf("rbf: the input vector h
