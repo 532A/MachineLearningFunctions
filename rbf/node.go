1298a71f4228eb53a733ec2cda776e44950a7e88
532A <101133899+532A@users.noreply.github.com>
2023-03-13 08:03:34 +0000

    xen/bootup: allow {read|write}_cr8 pvops call.

--- rbf/node.go
+++ rbf/node.go
@@ -37,4 +37,4 @@ func (n Node) String() string {
 // Calculate the output of the node.
 func (n *Node) Calculate(input []float64) (op []float64, err error) {
 	if len(n.InputWeights) != len(input) {
-		err = fmt.Errorf("rbf: the input vector has a length of %d values and should have the same numbe
+		err = fmt.Errorf("rbf: the input vector has a length of %d values and should have the same number of input weights, but we have %d node input we
