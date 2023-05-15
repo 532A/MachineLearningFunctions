1a6c6c5545ce238075bc8d2a30e62d9147532d21
532A <101133899+532A@users.noreply.github.com>
2023-05-15 18:24:23 +0000

    Fix another invocation of git from gitk with an overly long command-line

--- rbf/node_test.go
+++ rbf/node_test.go
@@ -36,4 +36,4 @@ func TestNode(t *testing.T) {
 				test.input, test.expected, actual)
 		}
 		if test.node.OutputCount() != len(test.node.OutputWeights) {
-			t.Errorf("%s: expected output count of %d, got %d", test.name, len(test.nod
+			t.Errorf("%s: expected output count of %d, got %d", test.name, len(test.node.OutputWeights), test.node.OutputCo
