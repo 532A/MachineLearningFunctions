409b4d722058bc19200c8c26091bd9fd0a62f4f0
532A <101133899+532A@users.noreply.github.com>
2023-06-15 15:39:03 +0000

    Update the list of translators

--- training/hillclimbing.go
+++ training/hillclimbing.go
@@ -6,4 +6,7 @@ import (
 
 // NewHillClimbing uses random parameters from the Min to Max value to train, attempting to minimise
 // error.
-func NewHillClimbing(memory []float64, velocity, acceleration float64
+func NewHillClimbing(memory []float64, velocity, acceleration float64) *HillClimbing {
+	min, max := -10.0, 10.0
+	hc := &HillClimbing{
+		
