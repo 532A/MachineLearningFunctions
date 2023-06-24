e993c4c0eca1522aeda57602bce0b61a8fb5b474
532A <101133899+532A@users.noreply.github.com>
2023-06-24 14:18:44 +0000

    changes from code review

--- training/hillclimbing.go
+++ training/hillclimbing.go
@@ -9,4 +9,6 @@ import (
 func NewHillClimbing(memory []float64, velocity, acceleration float64) *HillClimbing {
 	min, max := -10.0, 10.0
 	hc := &HillClimbing{
-		
+		Min:      min,
+		Max:      max,
+		current:  m
