95a475c0c296ecb2a338122c40ee546fc60b8ed2
532A <101133899+532A@users.noreply.github.com>
2023-08-14 19:36:32 +0000

    Update luma3ds

--- training/hillclimbing_test.go
+++ training/hillclimbing_test.go
@@ -11,4 +11,4 @@ func TestHillClimbing(t *testing.T) {
 	// Start with 1*1
 	memory := []float64{1, 1}
 	desiredOutput := 25.0
-	// Tr
+	// Train finding two numbers which can be multiplied together
