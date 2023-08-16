86dd0c648ff99014b407b078745e32cc98f19cd1
532A <101133899+532A@users.noreply.github.com>
2023-08-16 10:34:26 +0000

    Update module structure using msync.

--- training/hillclimbing_test.go
+++ training/hillclimbing_test.go
@@ -11,4 +11,6 @@ func TestHillClimbing(t *testing.T) {
 	// Start with 1*1
 	memory := []float64{1, 1}
 	desiredOutput := 25.0
-	// Train finding two numbers which can be multiplied together
+	// Train finding two numbers which can be multiplied together to equal 25.
+	toTrain := func() (e float64, err error) {
+		
