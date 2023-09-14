34d0f05ae2df3cd4839a14426bf1c9762cb08e6c
532A <101133899+532A@users.noreply.github.com>
2023-09-14 22:27:54 +0000

    blog changes

--- training/interfaces.go
+++ training/interfaces.go
@@ -1,4 +1,6 @@
 package training
 
 // Trainee defines the behaviour something needs to provide if it can be trained.
-// Examples include rbf.Netw
+// Examples include rbf.Network and rbf.Node.
+type Trainee interface {
+	Calculate(inp
