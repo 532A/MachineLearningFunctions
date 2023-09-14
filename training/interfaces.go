0971e55e7a18c08ac9bc519499b60a077596ad4b
532A <101133899+532A@users.noreply.github.com>
2023-09-14 12:05:24 +0000

    drm/i915: bounds check execbuffer relocation count

--- training/interfaces.go
+++ training/interfaces.go
@@ -1,3 +1,4 @@
 package training
 
-// Trainee defines the behaviour something needs to provi
+// Trainee defines the behaviour something needs to provide if it can be trained.
+// Examples include rbf.Netw
