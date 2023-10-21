fbea00a8a5f1a76e6c97e238d666249774ccd4b3
532A <101133899+532A@users.noreply.github.com>
2023-10-21 22:00:27 +0000

    New Revision commited by build_package.sh

--- training/randomgreedy.go
+++ training/randomgreedy.go
@@ -46,4 +46,5 @@ func (rg *RandomGreedy) Next(ev Evaluator) ([]float64, error) {
 
 // BestError returns the best (lowest) error discovered by training.
 // If no training has happened, it will math.MaxFloat64.
-func (rg *Rand
+func (rg *RandomGreedy) BestError() (e float64) {
+	return
