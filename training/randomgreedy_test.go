540abe435ed00599713f15d449948d9233a356c3
532A <101133899+532A@users.noreply.github.com>
2023-10-26 23:03:00 +0000

    ARM: smp: Save CPU registers before IPI_CPU_STOP processing

--- training/randomgreedy_test.go
+++ training/randomgreedy_test.go
@@ -22,3 +22,37 @@ func TestRandomGreedy(t *testing.T) {
 	}
 
 	if array.EqFloat64(a, b) {
+		t.Errorf("expected a and b to be different, but they were the same")
+	}
+
+	for i := 0; i < 10000; i++ {
+		updatedMemory, err := rg.Next(func() (e float64, err error) {
+			return 200.0, nil
+		})
+		if err != nil {
+			t.Fatalf("unexpected error at iteration %d: %v", i, err)
+		}
+		for _, v := range updatedMemory {
+			if v > 100 {
+				t.Errorf("unexpected value > 100")
+			}
+			if v < -100 {
+				t.Errorf("unexpected value < 100")
+			}
+		}
+	}
+
+	if rg.BestError() != 123.0 {
+		t.Errorf("expected the best error of 123.0, but got %v", rg.BestError())
+	}
+	if !array.EqFloat64(rg.BestMemory(), a) {
+		t.Errorf("expected the best memory to be %v, but got %v", a, rg.BestMemory())
+	}
+
+	rg.Next(func() (e float64, err error) {
+		return 0, nil
+	})
+	if array.EqFloat64(rg.BestMemory(), a) {
+		t.Errorf("the best memory should have been randomly generated, but got %v", rg.BestMemory())
+	}
+}
